2bea424e005a4e67191b3b1276be14a93a5df6cb
OmarEinea <eineao@gmail.com>
2016-06-12 18:25:39 +0300

    Functions to get and manage books

--- SearchBooks.py
+++ SearchBooks.py
@@ -18,8 +18,43 @@ class SearchBooks:
         self.br.set_handle_redirect(True)
         self.br.set_handle_referer(True)
         self.br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-        # An array for scrapped lists
+        # An array for scrapped lists and books
         self.lists = set()
+        self.books = set()
+
+    # Scrape books and write them to a file
+    def write_books(self, file_name="books.txt"):
+        books = open(file_name, 'w')
+        # Loop through book ids and write them
+        for book_id in self.get_books():
+            print >> books, book_id
+        books.close()
+
+    # Read already scraped books to the array
+    def read_books(self, file_name="books.txt"):
+        books = open(file_name, 'r')
+        # Loop through all books ids from file
+        for book_id in books:
+            # Add book id to array without new line
+            self.books.add(book_id.translate(None, '\n'))
+        books.close()
+        return self.books
+
+    # Main function to scrape books ids
+    def get_books(self):
+        # Return books array if it's not empty
+        if self.books != set():
+            return self.books
+        # Loop through all lists
+        for list_id in self.get_lists():
+            # Open each list url
+            self.br.open(self.site_url + "/list/show/" + list_id)
+            # Scrape pages until there's no next page
+            while True:
+                self.__scrape_list("^/book/show/", self.books)
+                if not self.__has_next_page("Next"):
+                    break
+        return self.books
 
     # Main function to scrape lists ids
     def get_lists(self):
