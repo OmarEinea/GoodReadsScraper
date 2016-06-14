1124ad1ea5d8c68203033347be63120b24317d98
OmarEinea <eineao@gmail.com>
2016-06-14 16:33:09 +0300

    Heavily document previous changes

--- Browser.py
+++ Browser.py
@@ -7,38 +7,46 @@ from selenium.common.exceptions import NoSuchElementException
 
 # A GoodReads specific PhantomJS browser class
 class GoodReadsBrowser(PhantomJS):
-    # Store GoodReads.com root url
-    site_url = "https://www.goodreads.com"
-
     def __init__(self):
         # Call super class constructor
         PhantomJS.__init__(self, "./phantomjs")
         self.next_page = None
 
-    # Shortcut to open GoodReads books list
+    # General shortcut to open a GoodReads page
+    def open(self, sub_url, keyword):
+        self.get("https://www.goodreads.com" + sub_url + str(keyword))
+
+    # Shortcut to open GoodReads book page
     def open_book(self, book_id):
-        self.get(self.site_url + "/book/show/" + str(book_id))
+        self.open("/book/show/", book_id)
 
     # Shortcut to open GoodReads books list
     def open_list(self, list_id):
-        self.get(self.site_url + "/list/show/" + str(list_id))
+        self.open("/list/show/", list_id)
 
     # Shortcut to open a GoodReads search page for books lists
     def open_list_search(self, keyword):
-        self.get(self.site_url + "/search?search_type=lists&q=" + str(keyword))
+        self.open("/search?search_type=lists&q=", keyword)
 
+    # Check if there's a next page
     def has_next_page(self):
+        # Try to find the next button
         try:
             self.next_page = self.find_element_by_class_name("next_page")
+        # Return false if there isn't one
         except NoSuchElementException:
             return False
+        # Check if button is click-able (i.e. it's anchor tag)
         return self.next_page.tag_name == 'a'
 
+    # Click the next button to go to next page
     def goto_next_page(self, repeated=False):
-        if not repeated:
+        # If nothing is repeated and there's next button
+        if not repeated and self.next_page:
             self.next_page.click()
             return True
         return False
 
+    # Get all links in page that has css class "XTitle"
     def links(self, title):
         return self.find_elements_by_class_name(title + "Title")
--- Reviews.py
+++ Reviews.py
@@ -7,57 +7,90 @@ from Browser import GoodReadsBrowser
 import time, os, codecs
 
 
+# A class to Scrape books Reviews from GoodReads.com
 class Reviews:
     def __init__(self):
         self.br = GoodReadsBrowser()
         self.ids = []
+        # Counter for invalid reviews
         self.invalid = 0
+        # Counter for reviews from different languages
         self.diff_lang = 0
+        # Flag for indicating reviews
+        # repetition due to page reload delay
         self.repeated = False
+        # Write book reviews to this file
         self.file = None
+        # Folder to put files in
         self.dir = "./BooksReviews/"
 
+    # Scrape and write one book's reviews to a file
     def write_book_reviews(self, book_id):
+        # Open book page by Id
         self.br.open_book(book_id)
+        # Create folder if it isn't already there
         if not os.path.exists(self.dir):
             os.makedirs(self.dir)
+        # Open the book reviews file with utf-8 encoding
         self.file = codecs.open(self.dir + str(book_id) + ".txt", "a+", "utf-8")
+        # Scrape as many reviews as possible
         while True:
             self._scrape_book(self.br.page_source)
+            # Stop if there're many invalid reviews or there's no next page
             if self.invalid > 5 or self.diff_lang > 10 or not self.br.has_next_page():
                 break
+            # Go to next page if there's no repetition
             if not self.br.goto_next_page(self.repeated):
                 self.repeated = False
+            # Wait for two second for next page to load
             time.sleep(2)
 
+    # Scrape a single page's reviews
     def _scrape_book(self, html):
+        # Store reviews section of the page in soup
         soup = BeautifulSoup(html, "lxml").find(id="bookReviews")
+        # Loop through reviews individually
         for review in soup.find_all(class_="bodycol"):
+            # Hold rating and text part of a review
             rating = review.find(class_="staticStars")
             readable = review.find(class_="readable")
+            # If one of the above is missing
             if not rating or not readable:
+                # Count it as invalid
                 self.invalid += 1
                 continue
+            # If it's not a strike of invalid reviews, reset the counter
             self.invalid = 0
+            # Get full review text, even hidden parts
             comment = (readable.find(style="display:none") or readable.find()).text
+            # Detect which language the review is in
             try:
                 if detect(comment) != "ar":
                     raise Exception
+            # Count it as a different language review
             except:
                 self.diff_lang += 1
                 continue
+            # If it's not a strike, reset the counter
             self.diff_lang = 0
+            # Hold how many stars the review was rated
             stars = str(self.SWITCH[rating.find().text])
+            # Write the scraped data to the file
             self._write_review(stars, comment, readable.get("id")[19:])
 
+    # Write data to file and prompt notification
     def _write_review(self, stars, comment, review_id):
+        # If review id is already stored among ids
         if review_id in self.ids:
+            # Notify and flag as repeated
             print("Repeated ID:", end=" ")
             self.repeated = True
         else:
+            # Otherwise notify and store as new
             self.ids.append(review_id)
             self.file.write(stars + ' ' + comment + '\n')
             print("Added ID:", end=" ")
         print(review_id)
 
+    # Switch reviews ratings to stars from 1 to 5
     SWITCH = {"it was amazing": 5, "really liked it": 4, "liked it": 3, "it was ok": 2, "did not like it": 1}
