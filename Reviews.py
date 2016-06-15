90797d3a2b9d91d205d5f2f6390d1db64755c248
OmarEinea <eineao@gmail.com>
2016-06-15 21:47:09 +0300

    Manage many books and writing

--- Reviews.py
+++ Reviews.py
@@ -21,6 +21,27 @@ class Reviews:
         # Folder to put files in
         self.dir = "./BooksReviews/"
 
+    # Setter for output path to write files in
+    def set_output_folder(self, path):
+        self.dir = path
+
+    # Scrape and write books' reviews to separate files
+    def write_books_reviews(self, books_ids, consider_previous=True):
+        if consider_previous:
+            # Loop through files in the chosen path
+            for file in os.listdir(self.dir):
+                # If file starts with digit (so not with C_ or E_)
+                if file[0].isdigit():
+                    # Then program was interrupted, delete file
+                    os.remove(file)
+                else:
+                    # Otherwise, don't scrape the book again
+                    books_ids.discard(file[2:-4])
+        # Loop through book ids in array and scrape books
+        for book_id in books_ids:
+            print("Scraping book: " + book_id)
+            self.write_book_reviews(book_id)
+
     # Scrape and write one book's reviews to a file
     def write_book_reviews(self, book_id):
         # Open book page by Id
@@ -46,6 +67,8 @@ class Reviews:
                 break
             # Wait for two second for next page to load
             time.sleep(2)
+        # Done writing, rename file accordingly
+        self.flag_file_name(str(book_id) + ".txt")
         # Empty ids array
         self.ids.clear()
 
@@ -95,3 +118,13 @@ class Reviews:
 
     # Switch reviews ratings to stars from 1 to 5
     SWITCH = {"it was amazing": 5, "really liked it": 4, "liked it": 3, "it was ok": 2, "did not like it": 1}
+
+    # Rename done files to complete or empty
+    def flag_file_name(self, name):
+        # If no reviews were added
+        if len(self.ids) == 0:
+            # Rename as an empty file
+            os.rename(self.dir + name, self.dir + "E_" + name)
+        else:
+            # Otherwise, rename as a complete file
+            os.rename(self.dir + name, self.dir + "C_" + name)
