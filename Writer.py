53e74af8f853fd8bd9565d97806180a681fea4d7
OmarEinea <eineao@gmail.com>
2016-06-17 17:26:30 +0300

    Better Opening and closing files

--- Writer.py
+++ Writer.py
@@ -13,36 +13,43 @@ class Writer:
         self.path = "./BooksReviews/"
         # Output file format
         self.format = ".txt"
+        self.empty = None
 
     # Path setter
     def set_path(self, path):
         self.path = path
 
-    # Discard already written files and delete incomplete ones
+    # Discard empty and complete files
     def consider_written_files(self, array):
+        self.prepare_path()
         # Loop through files in the chosen path
         for file in os.listdir(self.path):
-            # If file starts with digit (so not with C_ or E_)
-            if file[0].isdigit():
-                # Then program was interrupted, delete file
-                os.remove(self.path + file)
-            else:
-                # Otherwise, remove it from array
+            # If file starts with C_ or E_
+            if not file[0].isdigit():
+                # Try to remove it from array
                 try:
                     array.remove(file[2:-4])
                 except: pass
 
+    # Create folder if it isn't already there
+    def prepare_path(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+
     # General shortcut to open a file for writing
-    def open(self, file, key='w', path="./"):
-        self.file = codecs.open(path + file + self.format, key, "utf-8")
+    def open(self, name, key='w', path="./"):
+        self.file = codecs.open(path + name + self.format, key, "utf-8")
 
     # Open file to write book reviews
     def open_book_file(self, name):
-        # Create folder if it isn't already there
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
+        self.prepare_path()
+        # Delete file if it already exists
+        if os.path.exists(self.path + str(name) + self.format):
+            os.remove(self.path + str(name) + self.format)
         # Open the book reviews file with utf-8 encoding
         self.open(str(name), "a+", self.path)
+        # File is empty once you open it
+        self.empty = True
 
     # General shortcut to write a string to file
     def write(self, string):
@@ -51,6 +58,9 @@ class Writer:
     # Write review to file
     def write_review(self, id_, date, stars, comment):
         self.write(id_ + '\t' + date + '\t' + str(stars) + '\t' + comment)
+        # If a review is written, file isn't empty anymore
+        if self.empty:
+            self.empty = False
 
     # Write book meta data to file
     def write_book_meta(self, book_id, title, rating, id_, name):
@@ -61,14 +71,10 @@ class Writer:
         self.file.close()
 
     # Flag file as empty or complete and close it
-    def close_book_file(self, length):
-        # If no reviews were added
-        if length == 0:
-            # Rename as an empty file
-            os.rename(self.file.name, self.path + "E_" + self.file.name.split(self.path)[1])
-        else:
-            # Otherwise, rename as a complete file
-            os.rename(self.file.name, self.path + "C_" + self.file.name.split(self.path)[1])
+    def close_book_file(self):
+        # If no reviews were added mark file as empty, otherwise mark it as complete
+        os.rename(self.file.name, self.path + ("C_", "E_")[self.empty] + self.file.name.split(self.path)[1])
+        # Close file
         self.close()
 
     # Read already scraped books to the array
